63b69cecc08725237782da7bc4c5d796215d9352
Tim Newsome <tim@sifive.com>
2020-06-25 15:34:48 -0700

    ndsv5: Accept dmstatus.version==3 (0.14) (#489)
    Fixes #485.
    
    Change-Id: I60b3d68827ca726558bc28035c0b74c5cf0d9754
    Signed-off-by: Hellosun Wu <wujiheng.tw@gmail.com>

--- src/target/nds_vtarget.c
+++ src/target/nds_vtarget.c
@@ -329,8 +329,9 @@ static int riscv_examine(struct target *target)
 	info->dtmcontrol_idle = get_field(dtmcontrol, DTM_DTMCS_IDLE);
 
 	uint32_t dmstatus = dmi_read(target, DMI_DMSTATUS);
-	if (get_field(dmstatus, DMI_DMSTATUS_VERSION) != 2) {
-		LOG_ERROR("OpenOCD only supports Debug Module version 2, not %d "
+	if (get_field(dmstatus, DMI_DMSTATUS_VERSION) != 2 ||
+	    get_field(dmstatus, DMI_DMSTATUS_VERSION) != 3) {
+		LOG_ERROR("OpenOCD only supports Debug Module version 2 or 3, not %d "
 				"(dmstatus=0x%x)", get_field(dmstatus, DMI_DMSTATUS_VERSION), dmstatus);
 		return ERROR_FAIL;
 	}
